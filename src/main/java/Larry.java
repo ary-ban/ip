df1ccde49d4ffaef82989d74057def15c1bbc113
ary-ban <aryamanb15@gmail.com>
2025-09-05 18:46:29 +0800

    Level-2

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -1,5 +1,8 @@
 package larry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Larry {
 
 
@@ -12,20 +15,29 @@ public class Larry {
         System.out.println(" Bye. Hope to see you again soon!");
     }
 
-    private static void runEchoLoop() {
+    private static void runTaskLoop() {
         Scanner sc = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
+
         while (sc.hasNextLine()) {
             String input = sc.nextLine().trim();
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else if (!input.isEmpty()) {
+                tasks.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
     }
 
     public static void main(String[] args) {
         greet();
-        runEchoLoop();
+        runTaskLoop();
         exit();
     }
 }
