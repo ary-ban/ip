2c37e2dc7724c8d28e47d5a1321ac3b8e50c1041
ary-ban <aryamanb15@gmail.com>
2025-10-02 20:38:39 +0800

    Fix: force JavaFX software renderer on launch

--- src/main/java/larry/gui/Launcher.java
+++ src/main/java/larry/gui/Launcher.java
@@ -2,6 +2,8 @@ package larry.gui;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.setProperty("prism.order", "sw");
+        System.setProperty("javafx.platform", "gtk");
         Main.main(args);
     }
 }
